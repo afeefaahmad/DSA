218c6e01d7624a282cb3937bba4c742ed6812b2a
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2022-01-22 03:11:02 +0000

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,10 +1,11 @@
-//bubbleSort
-void bubbleSort( vector<int>, &arr, int n){
-    for (int i=1; i<=n; i++){
-        for(int j=1; j<n-1; j++){
-            if(arr[j]> arr[j+1]){
-                swap(arr[j], arr[j+1]);
+void selectionSort(vector<int>& arr, int n){
+	for(int i=0; i<n-1; i++){     //for rounds like round1 , round2, ...... round n-1
+        int minIndex=i;             //let the ith index element be the smallest 
+        for(int j=i+1; j<n; j++){     //For number of comparisons in each round
+            if(arr[minIndex]>arr[j]){    //In case any element at any index found to be smaller than ith index then swap it with minIndex
+				minIndex=j;	
             }
         }
+        swap(arr[minIndex], arr[i]);
     }
 }
